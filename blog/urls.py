809e1788200adaa66d9682fcba3c84e575fbf0d3
dainpapa <dainfafa@gmail.com>
2021-12-15 16:48:46 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
 #이부분을 채운다.
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('', views.PostList.as_view()),
+    #path('<int:pk>/', views.single_post_page),
+    #path('', views.index),
 
 ]
